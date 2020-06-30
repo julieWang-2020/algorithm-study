f1fb16ff21cd3c3a0bdda392e6f97a78655b29e1
wzh <512105256@qq.com>
2020-06-30 10:14:22 +0800

    快速排序

--- src/main/java/com/wzh/sort/SortAlgorithmCheck.java
+++ src/main/java/com/wzh/sort/SortAlgorithmCheck.java
@@ -13,55 +13,83 @@ public class SortAlgorithmCheck {
 
     public static void main(String[] args) {
         SortAlgorithmCheck check=new SortAlgorithmCheck();
-        check.sortCheck(SortTypeEnum.SORT_TYPE_SELECTION);
-        check.sortCheck(SortTypeEnum.SORT_TYPE_BUBBLE);
-        check.sortCheck(SortTypeEnum.SORT_TYPE_INSERTION);
-        check.sortCheck(SortTypeEnum.SORT_TYPE_SHELL);
-        check.sortCheck(SortTypeEnum.SORT_TYPE_MERGE);
-        check.sortCheck(SortTypeEnum.SORT_TYPE_QUICK);
+        check.checkOther(SortTypeEnum.SORT_TYPE_SELECTION);
+        check.checkOther(SortTypeEnum.SORT_TYPE_BUBBLE);
+        check.checkOther(SortTypeEnum.SORT_TYPE_INSERTION);
+        check.checkOther(SortTypeEnum.SORT_TYPE_SHELL);
+        check.checkOther(SortTypeEnum.SORT_TYPE_QUICK);
+        check.checkMerge();
     }
 
-    private void sortCheck(SortTypeEnum type) {
+    private void checkOther(SortTypeEnum type){
+        if(type.equals(SortTypeEnum.SORT_TYPE_MERGE)) throw new RuntimeException("not support sort");
         boolean same=true;
         for(int times=0;times<1000;times++){
-            int[] a1=generateRandomArray();
-            int[] a2=new int[a1.length];
-            System.arraycopy(a1,0,a2,0,a1.length);
+            int[] a=generateRandomArray(10000);
+            int[] copyA=sortCheck(type,a);
+            Arrays.sort(a);
 
-            Arrays.sort(a1);
-            switch (type){
-                case SORT_TYPE_SELECTION:
-                    SortAlgorithm.selectionSort(a2);
-                    break;
-                case SORT_TYPE_BUBBLE:
-                    SortAlgorithm.bubbleSort(a2);
-                    break;
-                case SORT_TYPE_INSERTION:
-                    SortAlgorithm.insertionSort(a2);
-                    break;
-                case SORT_TYPE_SHELL:
-                    SortAlgorithm.shellSort(a2);
-                    break;
-                case SORT_TYPE_MERGE:
-                    SortAlgorithm.mergeSort(a2,0,a2.length-1);
-                    break;
-                case SORT_TYPE_QUICK:
-                    SortAlgorithm.quickSort(a2,0,a2.length-1);
-                    break;
-                default:
-                    System.out.println(type.getValue()+" not exist");
-            }
-            for(int i=0;i<a2.length;i++){
-                if(a1[i]!=a2[i]) same=false;
+            for(int i=0;i<copyA.length;i++){
+                if(a[i]!=copyA[i]) same=false;
             }
         }
         System.out.println(same==true?type.getValue()+" Check right":type.getValue()+" Check wrong");
     }
 
-    private int[] generateRandomArray() {
+    private void checkMerge() {
+        boolean same=true;
+        for(int times=0;times<1000;times++){
+            int[] a51=generateRandomArray(5000);
+            Arrays.sort(a51);
+            int[] a52=generateRandomArray(5000);
+            Arrays.sort(a52);
+
+            int[] a=new int[10000];
+            System.arraycopy(a51,0,a,0,a51.length);
+            System.arraycopy(a52,0,a,5000,a52.length);
+
+            int[] copyA=sortCheck(SortTypeEnum.SORT_TYPE_MERGE,a);
+            Arrays.sort(a);
+            for(int i=0;i<copyA.length;i++){
+                if(a[i]!=copyA[i]) same=false;
+            }
+        }
+        System.out.println(same==true?SortTypeEnum.SORT_TYPE_MERGE.getValue()+" Check right":SortTypeEnum.SORT_TYPE_MERGE.getValue()+" Check wrong");
+    }
+    private int[]  sortCheck(SortTypeEnum type,int[] a) {
+
+        int[] copyA=new int[a.length];
+        System.arraycopy(a,0,copyA,0,a.length);
+
+        switch (type){
+            case SORT_TYPE_SELECTION:
+                SortAlgorithm.selectionSort(copyA);
+                break;
+            case SORT_TYPE_BUBBLE:
+                SortAlgorithm.bubbleSort(copyA);
+                break;
+            case SORT_TYPE_INSERTION:
+                SortAlgorithm.insertionSort(copyA);
+                break;
+            case SORT_TYPE_SHELL:
+                SortAlgorithm.shellSort(copyA);
+                break;
+            case SORT_TYPE_MERGE:
+                SortAlgorithm.mergeSort(copyA,0,copyA.length-1);
+                break;
+            case SORT_TYPE_QUICK:
+                SortAlgorithm.quickSort(copyA,0,copyA.length-1);
+                break;
+            default:
+                System.out.println(type.getValue()+" not exist");
+        }
+        return copyA;
+    }
+
+    private int[] generateRandomArray(int len) {
         Random r=new Random();
-        int[] a=new int[10000];
-        for(int i=0;i<10000;i++){
+        int[] a=new int[len];
+        for(int i=0;i<len;i++){
             a[i]=r.nextInt();
         }
         return a;
